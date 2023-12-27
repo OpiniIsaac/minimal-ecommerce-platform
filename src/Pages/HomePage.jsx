5187068d5f941277545dd55123b0da66749e6599
OpiniIsaac <isaacopini8@gmail.com>
2023-12-27 16:34:46 +0300

    done with nav bar

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,11 @@
+import { Container } from '@mui/material'
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+<NavBar/>
+    </>
   )
 }
